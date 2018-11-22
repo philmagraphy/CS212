430dccb3c845512ac5cdb7d6cbdd0b1f0bddf4fc
Phillip Ma <41809910+philmagraphy@users.noreply.github.com>
2018-11-21 23:21:43 -0500

    Add files via upload

---
+++ Project4/Project4.java
@@ -0,0 +1,19 @@
+package phillipma.project4;
+
+/**
+ * This part holds the main part of the program.
+ * GUI initialization is done here. Rest of the program is handled through the GUI file options.
+ * 
+ * @author Phillip Ma
+ * @since 2018-11-21
+ * for CS212
+ * Professor Kenneth Lord
+ * Lab Instructor Lin Zhao
+ */
+
+public class Project4 {
+
+    public static void main(String[] args) {
+        Date212GUI p1GUI = new Date212GUI();
+    }
+}
